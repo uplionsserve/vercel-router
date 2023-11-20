eee9b166752a82acf921d18ca42d410865c6194f
bouharri <bouharri@nmu.edu>
2023-11-20 09:35:05 -0500

    feat: added toKebabCase helper

--- payload-cms/src/collections/helpers.ts
+++ payload-cms/src/collections/helpers.ts
@@ -1,3 +1,15 @@
 export function isAdmin(user: any) {
 	return user.roles.admin
 }
+
+export function toKebabCase(str: string): string {
+	return (
+		str &&
+		str
+			.match(
+				/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+			)
+			.map((x) => x.toLowerCase())
+			.join("-")
+	)
+}
