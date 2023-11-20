1ce822c3a3f6c2182e37c1a0d09631516cf1dda4
Harrison Bouche <harrison@sm.media>
2023-11-20 12:45:07 -0500

    feat: HTML on globals!

--- payload-cms/src/collections/hunger/About.ts
+++ payload-cms/src/collections/hunger/About.ts
@@ -1,6 +1,7 @@
 import { GlobalConfig } from "payload/types"
+import { lexicalHTML } from "@payloadcms/richtext-lexical"
 
-const Home: GlobalConfig = {
+const About: GlobalConfig = {
 	slug: "hunger-pages-about",
 	label: "About",
 	admin: {
@@ -20,7 +21,9 @@ const Home: GlobalConfig = {
 			label: "Vision Statement",
 			type: "richText",
 		},
+		lexicalHTML("mission_statement", { name: "mission_statement_html" }),
+		lexicalHTML("vision_statement", { name: "vision_statement_html" }),
 	],
 }
 
-export default Home
+export default About
