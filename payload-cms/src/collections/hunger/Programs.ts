59c1546f4ab239db44ca1afaadaae0e4d8603ef3
Harrison Bouche <harrison@sm.media>
2023-11-21 11:26:25 -0500

    feat: logo and cta button for programs

--- payload-cms/src/collections/hunger/Programs.ts
+++ payload-cms/src/collections/hunger/Programs.ts
@@ -17,6 +17,12 @@ const Programs: CollectionConfig = {
 	defaultSort: "id",
 	fields: [
 		{
+			name: "logo_image",
+			label: "Logo Image",
+			type: "upload",
+			relationTo: "hunger-data-media",
+		},
+		{
 			name: "title",
 			type: "text",
 			required: true,
@@ -39,6 +45,32 @@ const Programs: CollectionConfig = {
 			name: "description",
 			type: "richText",
 			required: true,
+		},
+		{
+			name: "cta_button",
+			label: "Call To Action Button",
+			type: "group",
+			fields: [
+				{
+					name: "text",
+					type: "text",
+				},
+				{
+					name: "url",
+					type: "text",
+				},
+				{
+					name: "full_url",
+					type: "text",
+					hidden: true,
+					hooks: {
+						afterRead: [
+							({ data }) =>
+								`http://localhost:3000${data.cta_button.url}`,
+						],
+					},
+				},
+			],
 		},
 		lexicalHTML("description", { name: "description_html" }),
 	],
