fc94572c85fa4993dafef1aa0e22256b7002c1bb
Harrison Bouche <harrison@sm.media>
2023-11-27 20:14:53 -0500

    feat: UPLS branding

---
+++ payload-cms/src/components/Icon.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.css"
+
+export const Icon = () => (
+	<img className="nav-icon" src="/assets/icon-circle.png" />
+)
+
+export const Logo = () => (
+	<div className="logo">
+		<img src="/assets/logo-orangebg.png" alt="U.P. Lions Serve logo" />
+	</div>
+)
