df285b1ff535ab689311baf07fb53f3467dfe041
Harrison Bouche <harrison@sm.media>
2024-03-25 08:22:38 -0400

    fix: dev text showing with logo on production

--- payload-cms/src/components/Icon.tsx
+++ payload-cms/src/components/Icon.tsx
@@ -4,7 +4,9 @@ import "./style.css"
 export const Icon = () => (
 	<div className="icon-box">
 		<img className="nav-icon" src="/assets/icon-circle.png" />
-		<p className="dev-text">Dev Environment</p>
+		{process.env.NODE_ENV === "development" && (
+			<p className="dev-text">Dev Environment</p>
+		)}
 	</div>
 )
 
